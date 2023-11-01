b155cb402cdc63a4c91bff4616cf3e9322cd9d48
KabiPriya <kabipriya.t@gmail.com>
2023-11-01 21:03:12 +0530

    added layout

--- app/src/components/modal.tsx
+++ app/src/components/modal.tsx
@@ -1,19 +1,19 @@
-// import React from "react";
-// import { ModalProps } from "../type";
+import React from "react";
+import { ModalProps } from "../type";
 
-// const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
-//   return (
-//     isOpen && (
-//       <dialog open>
-//         <article>
-//           <header>
-//             <a aria-label="Close" className="close" onClick={onClose}></a>
-//           </header>
-//           {children}
-//         </article>
-//       </dialog>
-//     )
-//   );
-// };
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  return (
+    isOpen && (
+      <dialog open>
+        <article>
+          <header>
+            <a aria-label="Close" className="close" onClick={onClose}></a>
+          </header>
+          {children}
+        </article>
+      </dialog>
+    )
+  );
+};
 
-// export default Modal;
+export default Modal;
