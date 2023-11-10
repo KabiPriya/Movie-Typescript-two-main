0d47ad952844f7bff5a9446beddb043c53b7416b
KabiPriya <kabipriya.t@gmail.com>
2023-11-10 23:12:41 +0530

    added array

--- app/src/pages/Edit.tsx
+++ app/src/pages/Edit.tsx
@@ -1,54 +1,54 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { IEdit, IMovieAdd } from "../type";
-import Layout from "../components/layout";
-import { updateMovie } from "../services/api";
-import Form from "../components/MovieForm";
-import Modal from "../components/modal";
+// import React, { useEffect, useState } from "react";
+// import { useNavigate, useParams } from "react-router-dom";
+// import { IEdit, IMovieAdd } from "../type";
+// import Layout from "../components/layout";
+// import { updateMovie } from "../services/api";
+// import Form from "../components/MovieForm";
+// import Modal from "../components/modal";
 
-const EditForm: React.FC<IEdit> = ({ movie }) => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const editValue = {
-    title: movie.title,
-    year: movie.year,
-  };
+// const EditForm: React.FC<IEdit> = ({ movie }) => {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+//   const editValue = {
+//     title: movie.title,
+//     year: movie.year,
+//   };
 
   
 
-  useEffect(() => {
-    console.log("Getting info of ", id);
-  }, [id]);
+//   useEffect(() => {
+//     console.log("Getting info of ", id);
+//   }, [id]);
 
-  async function handleEditMovie(editedmovie: IMovieAdd) {
-    try {
-      const response = await updateMovie(editedmovie, movie.id);
-      console.log(response);
-      setIsModalOpen(true);
-      setEditError(null);
-    } catch (error) {
-      console.log(error);
-      setEditError("Error editing the movie.");
-    }
-  }
+//   async function handleEditMovie(editedmovie: IMovieAdd) {
+//     try {
+//       const response = await updateMovie(editedmovie, movie.id);
+//       console.log(response);
+//       setIsModalOpen(true);
+//       setEditError(null);
+//     } catch (error) {
+//       console.log(error);
+//       setEditError("Error editing the movie.");
+//     }
+//   }
 
  
 
-  return (
-    <>
-      <Layout title={`EditMovie${movie.title}`}>
-        <Form
-          handleAddMovie={handleEditMovie}
-          emptyMovie={editValue}
-          type="edit"
-        />
-      </Layout>
-{/* 
-      <Modal isOpen={isModalOpen} onClose={closeModalAndNavigate}>
-        {editError ? editError : "Successfully edited"} */}
-      </Modal>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <Layout title={`EditMovie${movie.title}`}>
+//         <Form
+//           handleAddMovie={handleEditMovie}
+//           emptyMovie={editValue}
+//           type="edit"
+//         />
+//       </Layout>
+// {/* 
+//       <Modal isOpen={isModalOpen} onClose={closeModalAndNavigate}>
+//         {editError ? editError : "Successfully edited"} */}
+//       </Modal>
+//     </>
+//   );
+// };
 
-export default EditForm;
+// export default EditForm;
