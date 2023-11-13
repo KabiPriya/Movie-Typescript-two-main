1276e2dd645747d5518c8ba2597731f80077a688
KabiPriya <kabipriya.t@gmail.com>
2023-11-13 23:06:27 +0530

    added read

--- app/src/pages/Edit.tsx
+++ app/src/pages/Edit.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-// import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 // import { IEdit, IMovieAdd } from "../type";
 // import Layout from "../components/layout";
 // import { updateMovie } from "../services/api";
